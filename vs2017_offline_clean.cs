3a69d8bd3aa6f3903e1b5b43705d1b5bfa8dbe1d
rise <rise.worlds@outlook.com>
2017-09-02 12:09:21 +0800

    add vs2017 offline setup update clean

---
+++ vs2017_offline_clean.cs
@@ -0,0 +1,51 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = Directory.GetCurrentDirectory();
+        if (path.EndsWith("\\") == false) path += "\\";
+        string catalog = path + "catalog.json";
+        if (File.Exists(catalog) == false)
+        {
+            Console.WriteLine("Please copy this program to the VS2017 offline folder.");
+            return;
+        }
+        string old = Directory.CreateDirectory(path + "- old -").FullName;
+        var o = fastJSON.JSON.ToDynamic(File.ReadAllText(catalog));
+
+        var folders = Directory.GetDirectories(path);
+        var packages = o["packages"];
+        foreach (var folder in folders)
+        {
+            var foldername = Path.GetFileName(folder);
+            var splitstring = foldername.Split(',');
+            bool found = false;
+            if (splitstring.Length > 1)
+            {
+                foreach (var package in packages)
+                {
+                    if (package["id"].ToString() == splitstring[0] && 
+                        package["version"] == splitstring[1].Split('=')[1].Trim())
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                found = true;
+                Console.WriteLine("not found in packages : " + foldername);
+            }
+
+            if (found == false)
+            {
+                // move directory
+                Console.WriteLine("moving folder : " + foldername);
+                Directory.Move(folder, old + "\\" + foldername);
+            }
+        }
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
